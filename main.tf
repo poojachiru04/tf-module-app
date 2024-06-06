1f84845c48571a8bbf1cda82be17e7611b17b7a9
pooja <poojaskumara@gmail.com>
2024-06-06 11:37:13 +0530

    add expense terraform to pipelines

--- main.tf
+++ main.tf
@@ -46,7 +46,18 @@ resource "aws_route53_record" "main" {
 
 resource "null_resource" "main" {
   depends_on = [aws_route53_record.main]
-  provisioner "local-exec" {
-    command = "sleep 120; cd /home/ec2-user/expense-ansible ; ansible-playbook -i ${aws_instance.main.private_ip}, -e ansible_user=ec2-user -e ansible_password=DevOps321 -e role_name=${var.name} -e env=dev expense.yml"
+
+  connection {
+    host     = aws_instance.main.private_ip
+    user     = "ec2-user"
+    password = "DevOps321"
+    type     = "ssh"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo labauto ansible",
+       "ansible-pull -i localhost, -U https://github.com/poojachiru04/expense-ansible -e role_name=${var.name} -e env=${var.env} expense.yml"
+    ]
   }
 }
